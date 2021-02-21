674625e1ed973ad637841c63fd736111f33dccea
Gerardo Reyes <gerardo.reyes@socialbakers.com>
2021-02-21 22:41:33 +0100

    draft

--- config/develop.js
+++ config/develop.js
@@ -2,22 +2,22 @@ module.exports = {
   api: {
     port: 8081,
     auth: {
-      cookieName: '',
-      domain: '',
-      scope: '',
-      audience: '',
-      origin: [''],
-      credentials: null,
+      cookieName: 'mandado-dev',
+      domain: 'happymandado.auth0.com',
+      scope: 'openid email profile',
+      audience: 'https://service.happymandado.com',
+      origin: ['http://happymandado.local:3000'],
+      credentials: true,
     },
   },
   db: {
-    driver: '',
-    scheme: '',
+    driver: 'mongoose',
+    scheme: 'mongodb',
     options: {
       useNewUrlParser: true,
     },
-    database: '',
-    host: '',
-    port: 0,
+    database: 'happy_mandado_dev',
+    host: 'localhost',
+    port: 27017,
   },
 };
