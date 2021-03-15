aa2950b9e5c4d43a59fe7789e64ec86cd5f916da
Gerardo Reyes <grevych@gmail.com>
2021-03-15 12:40:54 -0600

    Support multiple parameters in ES connection (#14)
     - Add script to generate product bulk for migrations

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,25 +1,28 @@
 module.exports = {
-  api: {
-    port: 'API_PORT',
-    dynamicPort: 'PORT',
-    auth: {
-      cookieName: 'API_AUTH_COOKIE_NAME',
-      domain: 'API_AUTH_DOMAIN',
-      scope: 'API_AUTH_SCOPE',
-      audience: 'API_AUTH_AUDIENCE',
-      origin: 'API_AUTH_ORIGIN',
-      credentials: 'API_AUTH_CREDENTIALS',
-    },
-  },
-  db: {
-    driver: 'DB_DRIVER',
-    scheme: 'DB_SCHEME',
-    options: {
-      useNewUrlParser: 'DB_OPTIONS_USE_NEW_URL_PARSER',
-    },
-    database: 'DB_DATABASE',
-    host: 'DB_HOST',
-    port: 'DB_PORT',
-    url: 'BONSAI_URL',
-  },
+	api: {
+		port: 'API_PORT',
+		dynamicPort: 'PORT',
+		auth: {
+			cookieName: 'API_AUTH_COOKIE_NAME',
+			domain: 'API_AUTH_DOMAIN',
+			scope: 'API_AUTH_SCOPE',
+			audience: 'API_AUTH_AUDIENCE',
+			origin: 'API_AUTH_ORIGIN',
+			credentials: 'API_AUTH_CREDENTIALS',
+		},
+	},
+	db: {
+		driver: 'DB_DRIVER',
+		scheme: 'DB_SCHEME',
+		options: {
+			useNewUrlParser: 'DB_OPTIONS_USE_NEW_URL_PARSER',
+		},
+		database: 'DB_DATABASE',
+		host: 'DB_HOST',
+		port: 'DB_PORT',
+		url: 'SEARCHBOX_SSL_URL',
+		user: 'DB_USER',
+		password: 'DB_PASSWORD',
+		apiKey: 'DB_API_KEY',
+	},
 };
